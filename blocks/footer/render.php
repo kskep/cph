4cca3128d5de0f121024adf824f3e8324a8e9698
kskep <130034485+kskep@users.noreply.github.com>
2026-04-02 22:19:27 +0300

    cap

--- blocks/footer/render.php
+++ blocks/footer/render.php
@@ -11,9 +11,6 @@ $defaults = array(
 
 $attrs = wp_parse_args( $attributes, $defaults );
 
-$menu_one_ref       = absint( $attrs['menuOneRef'] );
-$menu_two_ref       = absint( $attrs['menuTwoRef'] );
-$social_heading     = esc_html( $attrs['socialHeading'] );
 $promo_copy         = esc_html( $attrs['promoCopy'] );
 $promo_button_label = esc_html( $attrs['promoButtonLabel'] );
 $promo_button_url   = esc_url( $attrs['promoButtonUrl'] );
@@ -24,81 +21,8 @@ $wrapper_attributes = get_block_wrapper_attributes(
         'class' => 'cph-footer alignfull',
     )
 );
-
-$footer_nav_markup = static function( $ref, $fallback_links ) {
-    if ( $ref > 0 && get_post_type( $ref ) === 'wp_navigation' ) {
-        return do_blocks(
-            '<!-- wp:navigation {"ref":' . (int) $ref . ',"overlayMenu":"never","className":"cph-footer__nav","layout":{"type":"flex","orientation":"vertical"}} /-->'
-        );
-    }
-
-    $items = '';
-    foreach ( $fallback_links as $link ) {
-        $items .= '<!-- wp:navigation-link {"label":"' . esc_attr( $link['label'] ) . '","url":"' . esc_url_raw( $link['url'] ) . '","kind":"custom"} /-->';
-    }
-
-    return do_blocks(
-        '<!-- wp:navigation {"overlayMenu":"never","className":"cph-footer__nav","layout":{"type":"flex","orientation":"vertical"}} -->' .
-        $items .
-        '<!-- /wp:navigation -->'
-    );
-};
-
-$fallback_one = array(
-    array(
-        'label' => 'News',
-        'url'   => 'https://news.marriott.com/',
-    ),
-    array(
-        'label' => 'Hotel Development',
-        'url'   => 'https://hotel-development.marriott.com/brands/cph-hotels/',
-    ),
-    array(
-        'label' => 'Terms of Use',
-        'url'   => 'https://www.marriott.com/about/terms-of-use.mi',
-    ),
-);
-
-$fallback_two = array(
-    array(
-        'label' => 'Jobs',
-        'url'   => 'https://jobs.marriott.com/marriott/jobs?page=1&brand=CPH+Hotels',
-    ),
-    array(
-        'label' => 'About CPH Guestbook',
-        'url'   => 'https://www.instagram.com/citiplushotels/',
-    ),
-    array(
-        'label' => 'Privacy Statement',
-        'url'   => 'https://www.marriott.com/about/privacy.mi',
-    ),
-);
 ?>
 <footer <?php echo $wrapper_attributes; ?>>
-    <div class="cph-footer__inner alignwide">
-        <div class="wp-block-columns are-vertically-aligned-top cph-footer__columns">
-            <div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:18%">
-                <?php echo $footer_nav_markup( $menu_one_ref, $fallback_one ); ?>
-            </div>
-            <div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:18%">
-                <?php echo $footer_nav_markup( $menu_two_ref, $fallback_two ); ?>
-            </div>
-            <div class="wp-block-column is-vertically-aligned-top cph-footer__social-column" style="flex-basis:18%">
-                <h4 class="cph-footer__heading"><?php echo $social_heading; ?></h4>
-                <?php
-                echo do_blocks(
-                    '<!-- wp:social-links {"iconColor":"custom-off-black","iconColorValue":"#161717","size":"has-small-icon-size","className":"has-icon-color is-style-logos-only cph-footer__socials"} -->
-                    <!-- wp:social-link {"url":"https://www.instagram.com/citiplushotels/","service":"instagram"} /-->
-                    <!-- wp:social-link {"url":"https://www.facebook.com/citiplushotels","service":"facebook"} /-->
-                    <!-- wp:social-link {"url":"https://www.youtube.com/","service":"youtube"} /-->
-                    <!-- /wp:social-links -->'
-                );
-                ?>
-            </div>
-            
-        </div>
-    </div>
-   
     <div class="cph-footer__legal-strip alignfull">
         <p class="cph-footer__legal-copy"><?php echo $legal_copy; ?></p>
     </div>
--- patterns/footer.php
+++ patterns/footer.php
@@ -13,61 +13,7 @@
 ?>
 <!-- wp:group {"align":"full","className":"cph-footer","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull cph-footer"><!-- wp:group {"align":"wide","className":"cph-footer__inner cph-container","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide cph-footer__inner cph-container"><!-- wp:columns {"verticalAlignment":"top","className":"cph-footer__columns"} -->
-<div class="wp-block-columns are-vertically-aligned-top cph-footer__columns"><!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__menu-column"} -->
-<div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
-<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Quick Links</h4>
-<!-- /wp:heading -->
-
-<!-- wp:navigation {"overlayMenu":"never","className":"cph-footer__nav","layout":{"type":"flex","orientation":"vertical"}} -->
-<!-- wp:navigation-link {"label":"News","url":"https://news.marriott.com/","kind":"custom"} /-->
-<!-- wp:navigation-link {"label":"Hotel Development","url":"https://hotel-development.marriott.com/brands/cph-hotels/","kind":"custom"} /-->
-<!-- wp:navigation-link {"label":"Terms of Use","url":"https://www.marriott.com/about/terms-of-use.mi","kind":"custom"} /-->
-<!-- /wp:navigation --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__menu-column"} -->
-<div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
-<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Company</h4>
-<!-- /wp:heading -->
-
-<!-- wp:navigation {"overlayMenu":"never","className":"cph-footer__nav","layout":{"type":"flex","orientation":"vertical"}} -->
-<!-- wp:navigation-link {"label":"Jobs","url":"https://jobs.marriott.com/marriott/jobs?page=1&brand=CPH+Hotels","kind":"custom"} /-->
-<!-- wp:navigation-link {"label":"About CPH","url":"https://www.instagram.com/citiplushotels/","kind":"custom"} /-->
-<!-- wp:navigation-link {"label":"Privacy Statement","url":"https://www.marriott.com/about/privacy.mi","kind":"custom"} /-->
-<!-- /wp:navigation --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__social-column"} -->
-<div class="wp-block-column is-vertically-aligned-top cph-footer__social-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
-<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Stay Connected</h4>
-<!-- /wp:heading -->
-
-<!-- wp:social-links {"iconColor":"custom-off-black","iconColorValue":"#161717","size":"has-small-icon-size","className":"has-icon-color is-style-logos-only cph-footer__socials"} -->
-<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only cph-footer__socials"><!-- wp:social-link {"url":"https://www.instagram.com/citiplushotels/","service":"instagram"} /-->
-<!-- wp:social-link {"url":"https://www.facebook.com/citiplushotels","service":"facebook"} /-->
-<!-- wp:social-link {"url":"https://www.youtube.com/","service":"youtube"} /--></ul>
-<!-- /wp:social-links --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__contact-column"} -->
-<div class="wp-block-column is-vertically-aligned-top cph-footer__contact-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
-<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Contact Us</h4>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
-<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Address:</strong><br>123 Hotel Street<br>Rhodes, Greece 85100</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
-<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Phone:</strong><br>+30 123 456 7890</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
-<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Email:</strong><br>info@cityplushotels.gr</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+<div class="wp-block-group alignwide cph-footer__inner cph-container"></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"full","className":"cph-footer__legal-strip","layout":{"type":"constrained"}} -->
