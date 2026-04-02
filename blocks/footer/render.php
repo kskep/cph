b8531b9a2a6e88083d790b3085dfc67a572d2291
kskep <130034485+kskep@users.noreply.github.com>
2026-04-02 22:33:27 +0300

    test

--- blocks/footer/render.php
+++ blocks/footer/render.php
@@ -11,9 +11,7 @@ $defaults = array(
 
 $attrs = wp_parse_args( $attributes, $defaults );
 
-$promo_copy         = esc_html( $attrs['promoCopy'] );
-$promo_button_label = esc_html( $attrs['promoButtonLabel'] );
-$promo_button_url   = esc_url( $attrs['promoButtonUrl'] );
+$social_heading     = esc_html( $attrs['socialHeading'] );
 $legal_copy         = esc_html( $attrs['legalCopy'] );
 
 $wrapper_attributes = get_block_wrapper_attributes(
@@ -23,6 +21,40 @@ $wrapper_attributes = get_block_wrapper_attributes(
 );
 ?>
 <footer <?php echo $wrapper_attributes; ?>>
+    <div class="cph-footer__inner alignwide">
+        <div class="wp-block-columns are-vertically-aligned-top cph-footer__columns">
+            <div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%">
+                <h4 class="cph-footer__heading">Quick Links</h4>
+                <ul class="cph-footer__list">
+                    <li class="cph-footer__list-item">News</li>
+                    <li class="cph-footer__list-item">Hotel Development</li>
+                    <li class="cph-footer__list-item">Terms of Use</li>
+                </ul>
+            </div>
+            <div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%">
+                <h4 class="cph-footer__heading">Company</h4>
+                <ul class="cph-footer__list">
+                    <li class="cph-footer__list-item"><a class="cph-footer__list-link" href="/jobs">Jobs</a></li>
+                    <li class="cph-footer__list-item">About CPH</li>
+                    <li class="cph-footer__list-item">Privacy Statement</li>
+                </ul>
+            </div>
+            <div class="wp-block-column is-vertically-aligned-top cph-footer__social-column" style="flex-basis:25%">
+                <h4 class="cph-footer__heading"><?php echo $social_heading; ?></h4>
+                <ul class="cph-footer__list">
+                    <li class="cph-footer__list-item">Instagram</li>
+                    <li class="cph-footer__list-item">Facebook</li>
+                    <li class="cph-footer__list-item">YouTube</li>
+                </ul>
+            </div>
+            <div class="wp-block-column is-vertically-aligned-top cph-footer__contact-column" style="flex-basis:25%">
+                <h4 class="cph-footer__heading">Contact Us</h4>
+                <p class="cph-footer__contact-item"><strong>Address:</strong><br>123 Hotel Street<br>Rhodes, Greece 85100</p>
+                <p class="cph-footer__contact-item"><strong>Phone:</strong><br>+30 123 456 7890</p>
+                <p class="cph-footer__contact-item"><strong>Email:</strong><br>info@cityplushotels.gr</p>
+            </div>
+        </div>
+    </div>
     <div class="cph-footer__legal-strip alignfull">
         <p class="cph-footer__legal-copy"><?php echo $legal_copy; ?></p>
     </div>
--- patterns/footer.php
+++ patterns/footer.php
@@ -13,7 +13,55 @@
 ?>
 <!-- wp:group {"align":"full","className":"cph-footer","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull cph-footer"><!-- wp:group {"align":"wide","className":"cph-footer__inner cph-container","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide cph-footer__inner cph-container"></div>
+<div class="wp-block-group alignwide cph-footer__inner cph-container"><!-- wp:columns {"verticalAlignment":"top","className":"cph-footer__columns"} -->
+<div class="wp-block-columns are-vertically-aligned-top cph-footer__columns"><!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__menu-column"} -->
+<div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
+<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Quick Links</h4>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"cph-footer__list"} -->
+<ul class="wp-block-list cph-footer__list"><li class="cph-footer__list-item">News</li><li class="cph-footer__list-item">Hotel Development</li><li class="cph-footer__list-item">Terms of Use</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__menu-column"} -->
+<div class="wp-block-column is-vertically-aligned-top cph-footer__menu-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
+<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Company</h4>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"cph-footer__list"} -->
+<ul class="wp-block-list cph-footer__list"><li class="cph-footer__list-item"><a class="cph-footer__list-link" href="/jobs">Jobs</a></li><li class="cph-footer__list-item">About CPH</li><li class="cph-footer__list-item">Privacy Statement</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__social-column"} -->
+<div class="wp-block-column is-vertically-aligned-top cph-footer__social-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
+<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Stay Connected</h4>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"cph-footer__list"} -->
+<ul class="wp-block-list cph-footer__list"><li class="cph-footer__list-item">Instagram</li><li class="cph-footer__list-item">Facebook</li><li class="cph-footer__list-item">YouTube</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"25%","className":"cph-footer__contact-column"} -->
+<div class="wp-block-column is-vertically-aligned-top cph-footer__contact-column" style="flex-basis:25%"><!-- wp:heading {"level":4,"className":"cph-footer__heading","fontSize":"xsmall"} -->
+<h4 class="wp-block-heading cph-footer__heading has-xsmall-font-size">Contact Us</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
+<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Address:</strong><br>123 Hotel Street<br>Rhodes, Greece 85100</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
+<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Phone:</strong><br>+30 123 456 7890</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"cph-footer__contact-item","fontSize":"xsmall"} -->
+<p class="cph-footer__contact-item has-xsmall-font-size"><strong>Email:</strong><br>info@cityplushotels.gr</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"full","className":"cph-footer__legal-strip","layout":{"type":"constrained"}} -->
